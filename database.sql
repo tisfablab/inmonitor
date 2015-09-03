856dec876d2f2c2edb37f67fe7faf57922a161db
tisfablab <walter.weissensteiner@tis.bz.it>
2015-09-03 15:41:37 +0200

    compression of old values; data.php average bug

--- database.sql
+++ database.sql
@@ -1,48 +1,54 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (armv7l)
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: inmonitor
--- ------------------------------------------------------
--- Server version	5.5.44-0+deb7u1
+-- Host: localhost
+-- Generation Time: Sep 03, 2015 at 03:38 PM
+-- Server version: 5.5.44
+-- PHP Version: 5.4.44-0+deb7u1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `inmonitor`
+--
+CREATE DATABASE `inmonitor` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `inmonitor`;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `data`
 --
 
-DROP TABLE IF EXISTS `data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `data` (
+CREATE TABLE IF NOT EXISTS `data` (
   `device_id` tinyint(3) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `temperature` float(3,1) DEFAULT NULL,
   `humidity` float(3,1) DEFAULT NULL,
   `pressure` float(4,1) DEFAULT NULL,
   `illuminance` float(4,0) DEFAULT NULL,
+  `condensed` tinyint(4) NOT NULL,
   PRIMARY KEY (`device_id`,`timestamp`),
   KEY `timestamp` (`timestamp`),
-  KEY `device_id` (`device_id`)
+  KEY `device_id` (`device_id`),
+  KEY `condensed` (`condensed`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `devices`
 --
 
-DROP TABLE IF EXISTS `devices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `devices` (
+CREATE TABLE IF NOT EXISTS `devices` (
   `ID` tinyint(3) unsigned NOT NULL COMMENT 'inmonitor-x',
   `password` char(32) NOT NULL COMMENT 'used for data storage only',
   `last_ip` varchar(15) DEFAULT NULL COMMENT 'the ip this device was seen with last time',
@@ -50,14 +56,7 @@ CREATE TABLE `devices` (
   `private_ip` varchar(255) DEFAULT NULL COMMENT 'the device''s internal ip address',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-08-31 15:41:25
