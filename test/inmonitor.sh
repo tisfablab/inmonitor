84c1321f7ce0261b01225ec82e0216c5a1de23ee
tisfablab <walter.weissensteiner@tis.bz.it>
2015-09-09 15:25:29 +0200

    added wifi-reconnect script under test/

---
+++ test/inmonitor.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# /etc/init.d/inmonitor
+
+### BEGIN INIT INFO
+# Provides:          in-monitor
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts up the inmonitor scripts.
+# Description:       This service is used to gather data from different sources and send them to the cloud server/database. Furthermore, it adds an URL link to the local IP.
+### END INIT INFO
+
+MYPATH="/home/pi/inmonitor/python"
+SCRIPT="inmonitor.py"
+
+WIFIRECONNECT="/home/pi/inmonitor/wifi-reconnect.sh"
+
+case "$1" in 
+    start)
+        value=$( ps ax | grep "$WIFIRECONNECT" )
+	if [ $value -lt 2 ]
+	then
+		echo "Starting wifi reconnect script"
+		$WIFIRECONNECT > /dev/null 2>&1 &
+	fi
+	echo "Starting inmonitor"
+	$MYPATH/$SCRIPT >/dev/null 2>&1 &
+        ;;
+    test)
+        echo "(Re)Starting inmonitor in verbose mode"
+	killall $SCRIPT >/dev/null 2>&1
+        $MYPATH/$SCRIPT
+        ;;
+    stop)
+        echo "Stopping inmonitor"
+        killall $SCRIPT >/dev/null 2>&1
+        ;;
+    *)
+        echo "Usage: service inmonitor start|stop|test"
+        exit 1
+        ;;
+esac
+
+exit 0
+
---
+++ test/wifi-reconnect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INTERFACE="wlan0"
+
+if ! ifconfig $INTERFACE | grep -q "inet addr:"; then
+	echo "Script aborted: Network on $INTERFACE was down on startup."
+	exit 1
+fi
+
+while true ; do
+   if ifconfig $INTERFACE | grep -q "inet addr:" ; then
+      echo "Network is up. Not doing anything."
+   else
+      echo "Network connection down! Attempting reconnection."
+      ifup --force wlan0
+   fi
+   sleep 300
+done
+
